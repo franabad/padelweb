442c3a7b01ff0672eac903da6316766a264a40a3
fran_abad <fam1096@hotmail.com>
2023-04-04 01:05:16 +0200

    Tiny type fix

--- src/models/test.model.ts
+++ src/models/test.model.ts
@@ -2,9 +2,5 @@ export interface TestModel {
   id: string;
   userId: string;
   title: string;
-<<<<<<< HEAD
   completed: boolean;
-=======
-  completed: boolean; 
->>>>>>> dd0ebaf7570d2d124d6854838a9e9c7639ff4fec
 }
