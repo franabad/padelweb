a4c9914a82b4f9a20624d352b1512008cfef3c61
fran_abad <fam1096@hotmail.com>
2023-04-25 18:45:30 +0200

    Add the button

---
+++ src/components/client/button.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Button = () => {
+  return (
+    <button
+      id="login"
+      className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-3xl"
+    >
+      Iniciar sesión
+    </button>
+  );
+};
+
+export default Button;
