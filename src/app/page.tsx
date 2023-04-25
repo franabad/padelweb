1ee6932c988b9c73d075f84df4f0f3596a483b1d
fran_abad <fam1096@hotmail.com>
2023-04-25 18:44:51 +0200

    Add navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,17 @@
 import Navbar from "../components/client/navbar";
 import "./globals.css";
+import Image from "next/image";
+import fondo from "/assets/fondo.jpg";
 
 const Page = () => {
   return (
-    <div>
-      <Navbar />
+    <div
+      className="bg-hero bg-cover bg-center h-screen"
+      style={{ backgroundImage: `url(${fondo})` }}
+    >
+      <div>
+        <Navbar />
+      </div>
     </div>
   );
 };
