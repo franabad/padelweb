682f24bbe305999fb5510b79acd7b2d2d0032125
fran_abad <fam1096@hotmail.com>
2023-04-25 18:45:23 +0200

    Add navbar code

--- src/components/client/navbar.tsx
+++ src/components/client/navbar.tsx
@@ -7,16 +7,19 @@ import Button from "./button";
 
 const NavBar = () => {
   return (
-    <nav className="bg-slate-400">
+    <nav className="bg-black rounded-full mx-8 my-5">
       <div className="flex items-center justify-around">
         <ul className="flex uppercase items-center gap-80">
           <li className="mr-6">
-            <Link href="/" className="px-4 py-2 hover:text-white">
+            <Link href="/" className="px-4 py-2 text-white hover:text-blue-400">
               Inicio
             </Link>
           </li>
           <li className="mr-6">
-            <Link href="/about" className="px-4 py-2 hover:text-white">
+            <Link
+              href="/about"
+              className="px-4 py-2 text-white hover:text-blue-400"
+            >
               Nosotros
             </Link>
           </li>
@@ -37,7 +40,10 @@ const NavBar = () => {
             <NavLinks />
           </li>
           <li className="mr-6">
-            <Link href="/faq" className="px-4 py-2 hover:text-white">
+            <Link
+              href="/faq"
+              className="px-4 py-2 text-white hover:text-blue-400"
+            >
               FAQ
             </Link>
           </li>
