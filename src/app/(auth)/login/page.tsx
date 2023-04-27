ef39533e7c8be4300509b7e1a8a1464b27e406b2
fran_abad <fam1096@hotmail.com>
2023-04-27 14:30:20 +0200

    Add function to redirect home when session's started

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,7 +1,40 @@
+'use client'
 import LoginForm from '@/components/client/loginForm'
+import { useEffect, useState } from 'react'
+import Link from 'next/link'
 
 const LoginPage: React.FC = () => {
-  return <LoginForm />
+  const [loggedIn, setLoggedIn] = useState(false)
+
+  useEffect(() => {
+    const checkLoginStatus = async () => {
+      try {
+        const response = await fetch('http://localhost:4000/login', {
+          method: 'GET',
+          credentials: 'include'
+        })
+        if (response.ok) {
+          setLoggedIn(true)
+        }
+      } catch (error) {
+        console.error('Error al comprobar el estado de inicio de sesión: ', error)
+      }
+    }
+    // eslint-disable-next-line @typescript-eslint/no-floating-promises
+    checkLoginStatus()
+  }, [])
+  return (
+    <>
+      {loggedIn
+        ? (
+          <Link href="http://localhost:3000/" />
+          )
+        : (
+          <LoginForm />
+          )}
+    </>
+
+  )
 }
 
 export default LoginPage
