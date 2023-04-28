cac976a37428584013b88d55bdb9207d1e4c8014
Fran Abad <fam1096@hotmail.com>
2023-04-28 14:29:53 +0200

    Fixes login styling

--- src/components/client/loginForm.tsx
+++ src/components/client/loginForm.tsx
@@ -19,22 +19,27 @@ const LoginForm = () => {
         'Content-Type': 'application/json'
       },
       credentials: 'include'
-    }).then(async res => await res.json())
-      .then(data => { console.log(session) })
-      .catch(err => { console.log(err) })
+    })
+      .then(async (res) => await res.json())
+      .then((data) => {
+        console.log(session)
+      })
+      .catch((err) => {
+        console.log(err)
+      })
   }
 
   const { data: session } = useSession()
 
   return (
-    <section className="bg-gray-900/90">
+    <section className="bg-gray-900/80 backdrop-blur-md">
       <div className="flex flex-col items-center justify-center px-6 py-8 md:h-screen lg:py-0">
         <div className="w-full md:w-1/2 bg-cover bg-center bg-no-repeat bg-[url('../../public/assets/fondo.jpg')]"></div>
         <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
-            <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-              <h1 className="text-center text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-                Sign into your account
-              </h1>
+          <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+            <h1 className="text-center text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+              Sign into your account
+            </h1>
             <form
               className="space-y-4 md:space-y-6"
               // eslint-disable-next-line @typescript-eslint/no-misused-promises
@@ -42,9 +47,11 @@ const LoginForm = () => {
             >
               <div className="flex flex-col">
                 <div className="flex flex-col mb-3">
-                  <label htmlFor="email" className="text-white mb-3">Email</label>
+                  <label htmlFor="email" className="text-white mb-3">
+                    Email
+                  </label>
                   <input
-                    className='bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                     type="text"
                     id="email"
                     {...register('email', {
@@ -55,14 +62,16 @@ const LoginForm = () => {
                       }
                     })}
                   />
-                  {(errors.email != null) && (
+                  {errors.email != null && (
                     <span className="text-xs text-red-600 text-center">
                       Invalid email
                     </span>
                   )}
                 </div>
                 <div className="flex flex-col mb-3">
-                  <label htmlFor="password" className="text-white mb-3">Password</label>
+                  <label htmlFor="password" className="text-white mb-3">
+                    Password
+                  </label>
                   <input
                     className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                     type="password"
@@ -72,7 +81,12 @@ const LoginForm = () => {
                     })}
                   />
                 </div>
-                <button type="submit" className='mt-4 w-full text-white bg-indigo-800 hover:bg-indigo-900 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800'>Sign in</button>
+                <button
+                  type="submit"
+                  className="mt-4 w-full text-white bg-indigo-800 hover:bg-indigo-900 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                >
+                  Sign in
+                </button>
               </div>
             </form>
           </div>
