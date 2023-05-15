fb3660d89036ec0d795b72bc633a84da66077479
Fran Abad <fam1096@hotmail.com>
2023-05-15 22:39:18 +0200

    navigation from sign in

--- src/components/client/registerForm.tsx
+++ src/components/client/registerForm.tsx
@@ -3,8 +3,10 @@
 import '../../app/globals.css'
 import { useSession } from 'next-auth/react'
 import { useForm } from 'react-hook-form'
+import { useRouter } from 'next/navigation'
 
 const RegisterForm = () => {
+  const router = useRouter()
   const {
     register,
     handleSubmit,
@@ -19,9 +21,15 @@ const RegisterForm = () => {
         'Content-Type': 'application/json'
       },
       credentials: 'include'
-    }).then(async res => await res.json())
-      .then(data => { console.log(session) })
-      .catch(err => { console.log(err) })
+    })
+      .then(async (res) => await res.json())
+      .then((data) => {
+        console.log(session)
+        router.push('/')
+      })
+      .catch((err) => {
+        console.log(err)
+      })
   }
 
   const { data: session } = useSession()
@@ -30,10 +38,10 @@ const RegisterForm = () => {
     <section className="bg-slate-950/95">
       <div className="flex flex-col items-center justify-center px-6 py-8 md:h-screen lg:py-0">
         <div className="w-full bg-red-700 rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
-            <div className="p-6 space-y-4">
-              <h1 className="text-center text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-                Sign up
-              </h1>
+          <div className="p-6 space-y-4">
+            <h1 className="text-center text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+              Sign up
+            </h1>
             <form
               className="space-y-4 md:space-y-6"
               // eslint-disable-next-line @typescript-eslint/no-misused-promises
@@ -41,9 +49,11 @@ const RegisterForm = () => {
             >
               <div className="flex flex-col">
                 <div className="flex flex-col mb-3">
-                  <label htmlFor="email" className="text-white mb-3">Email</label>
+                  <label htmlFor="email" className="text-white mb-3">
+                    Email
+                  </label>
                   <input
-                    className='bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                     type="text"
                     id="email"
                     {...register('email', {
@@ -54,14 +64,16 @@ const RegisterForm = () => {
                       }
                     })}
                   />
-                  {(errors.email != null) && (
+                  {errors.email != null && (
                     <span className="text-xs text-red-600 text-center">
                       Invalid email
                     </span>
                   )}
                 </div>
                 <div className="flex flex-col mb-3">
-                  <label htmlFor="password" className="text-white mb-3">Password</label>
+                  <label htmlFor="password" className="text-white mb-3">
+                    Password
+                  </label>
                   <input
                     className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                     type="password"
@@ -72,7 +84,12 @@ const RegisterForm = () => {
                     })}
                   />
                 </div>
-                <button type="submit" className='mt-4 w-full text-white bg-indigo-800 hover:bg-indigo-900 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800'>Sign up</button>
+                <button
+                  type="submit"
+                  className="mt-4 w-full text-white bg-indigo-800 hover:bg-indigo-900 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                >
+                  Sign up
+                </button>
               </div>
             </form>
           </div>
