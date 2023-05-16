a7fe14dda22674a6d2d4fd50e7bc0d6a76239ea3
Fran Abad <fam1096@hotmail.com>
2023-05-16 11:26:36 +0200

    Add more options to the sign up

--- src/components/client/registerForm.tsx
+++ src/components/client/registerForm.tsx
@@ -49,6 +49,35 @@ const RegisterForm = () => {
             >
               <div className="flex flex-col">
                 <div className="flex flex-col mb-3">
+                  <label htmlFor="text" className="text-white mb-3">
+                    Nombre
+                  </label>
+                  <input
+                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    type="text"
+                    id="name"
+                    {...register('name', {
+                      required: 'required'
+                    })}
+                  />
+                  {errors.name != null && (
+                    <span className="text-base text-red-600 text-center mt-2">
+                      This field is required
+                    </span>
+                  )}
+                </div>
+                <div className="flex flex-col mb-3">
+                  <label htmlFor="text" className="text-white mb-3">
+                    Apellidos
+                  </label>
+                  <input
+                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    type="text"
+                    id="lastname"
+                    {...register('lastname')}
+                  />
+                </div>
+                <div className="flex flex-col mb-3">
                   <label htmlFor="email" className="text-white mb-3">
                     Email
                   </label>
@@ -65,7 +94,7 @@ const RegisterForm = () => {
                     })}
                   />
                   {errors.email != null && (
-                    <span className="text-xs text-red-600 text-center">
+                    <span className="text-base text-red-600 text-center mt-2">
                       Invalid email
                     </span>
                   )}
