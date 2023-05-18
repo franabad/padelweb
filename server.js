9acf0d570415ab64177963d4266a2f9278ab1a7a
Fran Abad <fam1096@hotmail.com>
2023-05-18 22:56:46 +0200

    Add and change files to deploy into ISS

--- server.js
+++ server.js
@@ -1,28 +1,37 @@
 const { createServer } = require('http')
-const { url } = require('url')
+// eslint-disable-next-line n/no-deprecated-api
+const { parse } = require('url')
 const next = require('next')
 
 const dev = process.env.NODE_ENV !== 'production'
 const port = process.env.PORT || 3000
+// when using middleware `hostname` and `port` must be provided below
 const app = next({ dev })
 const handle = app.getRequestHandler()
 
 app.prepare().then(() => {
   createServer((req, res) => {
-    // Be sure to pass `true` as the second argument to `url.parse`.
-    // This tells it to parse the query portion of the URL.
-    const parsedUrl = url(req.url, true)
-    const { pathname, query } = parsedUrl
+    try {
+      // Be sure to pass `true` as the second argument to `url.parse`.
+      // This tells it to parse the query portion of the URL.
+      const parsedUrl = parse(req.url, true)
+      const { pathname, query } = parsedUrl
 
-    if (pathname === '/a') {
-      app.render(req, res, '/a', query)
-    } else if (pathname === '/b') {
-      app.render(req, res, '/b', query)
-    } else {
-      handle(req, res, parsedUrl)
+      if (pathname === '/a') {
+        app.render(req, res, '/a', query)
+      } else if (pathname === '/b') {
+        app.render(req, res, '/b', query)
+      } else {
+        handle(req, res, parsedUrl)
+      }
+    } catch (err) {
+      console.error('Error occurred handling', req.url, err)
+      res.statusCode = 400
+      res.end('internal server error')
     }
-  }).listen(port, (err) => {
-    if (err) throw err
-    console.log(`> Ready on http://localhost:${port}`)
   })
+    .listen(port, (err) => {
+      if (err) throw err
+      console.log(`> Ready on http://localhost:${port}`)
+    })
 })
