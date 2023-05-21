e1fd9aedefbe3cffa89aa6c46de08cb485a77640
Fran Abad <fam1096@hotmail.com>
2023-05-22 01:26:40 +0200

    Add tailwind animated

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
- 
+    './app/**/*.{js,ts,jsx,tsx}',
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+
     // Or if using `src` directory:
-    "./src/**/*.{js,ts,jsx,tsx}",
+    './src/**/*.{js,ts,jsx,tsx}'
   ],
   theme: {
     extend: {
       colors: {
-        "primary": "#123123"
+        primary: '#123123'
       }
-    },
+    }
   },
-  plugins: [],
+  plugins: [
+    require('tailwindcss-animated')
+  ]
 }
-
