ad28989a09d6904159d09a0399e2c4fe5019ce08
Fran Abad <fam1096@hotmail.com>
2023-05-23 02:16:18 +0200

    Minor styling changes and add Profile Component

--- src/components/server/authButtons.tsx
+++ src/components/server/authButtons.tsx
@@ -2,22 +2,23 @@ import Link from 'next/link'
 import { authOptions } from '../../pages/api/auth/[...nextauth]'
 import { getServerSession } from 'next-auth/next'
 import LoginButton from '../client/loginButton'
+// import Profile from '../client/profile'
+import { type ReactNode } from 'react'
+import Profile from '../client/profileDropdown'
 
-const Auth = async () => {
+const Auth = async ({ children }: { children: ReactNode }) => {
   const session = await getServerSession(authOptions)
-
-  console.log('Sessión en authButtons', session)
 
   return (
     <div className="flex">
       {session?.user != null ? (
-        <p className="text-white">Mi perfil</p>
+        <Profile />
       ) : (
         <>
           <LoginButton />
           <Link
             id="register"
-            className="bg-transparent text-white hover:text-gray-300 hover:bg-slate-950/100 font-bold py-2 px-3 border border-white rounded-lg"
+            className="bg-transparent text-white hover:bg-slate-950/100 font-bold py-2 px-3 ring-1 ring-inset ring-gray-300/50 hover:ring-white rounded-lg"
             href="/register"
           >
             Sign up
