fb0e87dd389205ca2c70e886147f2b35fcfb1fb7
Fran Abad <fam1096@hotmail.com>
2023-05-23 02:15:32 +0200

    Update styling and new function email span when registered

--- src/components/client/registerForm.tsx
+++ src/components/client/registerForm.tsx
@@ -5,6 +5,7 @@ import { useRouter } from 'next/navigation'
 import { FormProvider, useForm } from 'react-hook-form'
 import '../../app/globals.css'
 import Input from './input'
+import { useState } from 'react'
 
 interface FormData {
   name: string
@@ -21,9 +22,10 @@ const apiBaseUrl = process.env.NEXT_PUBLIC_API_BASE_URL
 const RegisterForm = () => {
   const router = useRouter()
   const form = useForm<FormData>()
+  const [emailExists, setEmailExists] = useState<boolean>(false)
 
-  const onSubmit = (data: FormData) => {
-    fetch(`http://${apiBaseUrl}/register`, {
+  const onSubmit = async (data: FormData) => {
+    const res = await fetch(`http://${apiBaseUrl}/register`, {
       method: 'POST',
       body: JSON.stringify(data),
       headers: {
@@ -31,9 +33,9 @@ const RegisterForm = () => {
       },
       credentials: 'include'
     })
-      .then(async (res) => await res.json())
+    await res.json()
       .then(() => {
-        router.push('/')
+        if (res.status === 200) { router.push('/') } else if (res.status === 502) { setEmailExists(true) }
       })
       .catch((err) => {
         console.log(err)
@@ -44,7 +46,7 @@ const RegisterForm = () => {
     return (
       <button
         type="submit"
-        className="w-1/2 mt-7 text-white text-center text-lg bg-sky-800 hover:bg-sky-900 py-4 rounded-3xl"
+        className="py-4 px-10 mt-10 text-white text-center text-lg bg-sky-800/70 hover:bg-sky-900/50 rounded-2xl"
       >
         {children}
       </button>
@@ -52,76 +54,78 @@ const RegisterForm = () => {
   }
 
   return (
-    <div className="bg-slate-950 flex items-center h-full justify-end">
-      <header className="min-h-screen w-1/2 justify-start items-start flex">
-        <div className="mt-64 ml-44">
+    <div className="flex items-center h-full justify-end">
+      <header className="bg-[url('../../public/assets/banner.jpg')] bg-cover bg-no-repeat bg-left-top min-h-screen w-4/6 justify-center items-center flex opacity-70">
+        <div className="z-10">
           <h1 className="text-8xl flex flex-col font-semibold tracking-tigh text-white">
             E-sports <span className="text-yellow-600">Center</span>
           </h1>
-          <h2 className="mt-2 text-3xl text-white flex flex-col">
-            El sitio en el que puedes hacer lo que quieras,{' '}
+          <h2 className="mt-6 text-3xl text-white flex flex-col">
+            El sitio en el que puedes hacer
+            <span className="text-yellow-600">lo que quieras,</span>
             <span className="text-red-600">cuando quieras</span>
           </h2>
         </div>
       </header>
       <main className="bg-slate-950 flex flex-col items-center justify-center min-h-screen w-1/2">
-        <h1 className="text-4xl font-bold mb-10 tracking-tigh text-white">
-          Create your account
-        </h1>
+        <div className='w-[423.58px]'>
+          <h1 className="text-4xl font-bold mb-24 tracking-tigh text-white">
+            Create your account
+          </h1>
+          <FormProvider {...form}>
+            <form
+              className="space-y-5 w-full flex flex-col h-full"
+              // eslint-disable-next-line @typescript-eslint/no-misused-promises
+              onSubmit={form.handleSubmit(onSubmit)}
+            >
+              <Input
+                name="name"
+                label="Nombre"
+                type="text"
+                placeholder="Taylor"
+                validations={{ required: true }}
+                errorMessage="Este campo es obligatorio"
+              />
 
-        <FormProvider {...form}>
-          <form
-            className="space-y-6 w-1/2"
-            // eslint-disable-next-line @typescript-eslint/no-misused-promises
-            onSubmit={form.handleSubmit(onSubmit)}
-          >
-            <Input
-              name="name"
-              label="Nombre"
-              type="text"
-              placeholder="Elber"
-              validations={{ required: true }}
-              errorMessage="Este campo es obligatorio"
-            />
+              <Input
+                name="lastname"
+                label="Apellido"
+                type="text"
+                placeholder="Hawkins"
+              />
 
-            <Input
-              name="lastname"
-              label="Apellido"
-              type="text"
-              placeholder="Galarga"
-            />
-
-            <Input
-              name="email"
-              label="Email"
-              type="text"
-              placeholder="elber@galarga.com"
-              validations={{
-                required: true,
-                pattern: /\S+@\S+\.\S+/
-              }}
-              errorMessage="Este campo es obligatorio"
-            />
-
-            <Input
-              name="password"
-              label="Password"
-              type="password"
-              placeholder="Password"
-              validations={{ required: true, minLength: 4 }}
-              errorMessage="Este campo es obligatorio"
-            />
-            <div className="items-end justify-end flex">
-              <Link
-                href="/login"
-                className="text-white hover:text-gray-400 underline cursor-pointer mr-5"
-              >
-                Have already an account? Log in
-              </Link>
-              <Button>Sign up</Button>
-            </div>
-          </form>
-        </FormProvider>
+              <Input
+                name="email"
+                label="Email"
+                type="text"
+                placeholder="example@domain.com"
+                validations={{
+                  required: true,
+                  pattern: /\S+@\S+\.\S+/
+                }}
+                errorMessage="Este campo es obligatorio"
+              />
+              {emailExists && (
+              <span className="text-red-600">El email introducido ya existe</span>
+              )}
+              <Input
+                name="password"
+                label="Password"
+                type="password"
+                placeholder="Password"
+                validations={{ required: true, minLength: 4 }}
+                errorMessage="Este campo es obligatorio"
+              />
+              <div className="items-end justify-between flex">
+                <div className="text-white flex flex-col items-start justify-start">
+                  Already have an account?
+                  <Link href= "/login" className="text-white hover:text-gray-400 underline cursor-pointer">Sign in</Link>
+                </div>
+                <Button>Sign up</Button>
+              </div>
+            </form>
+          </FormProvider>
+        </div>
       </main>
     </div>
   )
