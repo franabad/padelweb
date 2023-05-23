7f2a1dd7e28b9d0ef542292427d3861b28f5024a
Fran Abad <fam1096@hotmail.com>
2023-05-23 02:15:57 +0200

    Major change of the Services dropdown

--- src/components/client/services.tsx
+++ src/components/client/services.tsx
@@ -1,45 +1,91 @@
+'use client'
 import Link from 'next/link'
-import { useState } from 'react'
+import { Fragment } from 'react'
+import { Menu, Transition } from '@headlessui/react'
 
-const links = [
-  { title: 'Tenis', href: '/tenis' },
-  { title: 'Piscina', href: '/piscina' },
-  { title: 'Informática', href: '/informatica' },
-  { title: 'Restaurante', href: '/restaurante' }
-]
+function classNames(...classes: any[]) {
+  return classes.filter(Boolean).join(' ')
+}
 
-const Services = () => {
-  const [showServices, setShowServices] = useState<boolean>(false)
+const Profile = () => {
+  // const [showServices, setShowServices] = useState<boolean>(false)
 
   return (
-    <div
-      className="relative text-white hover:text-blue-700 hover:cursor-pointer"
-      onMouseEnter={() => {
-        setShowServices(true)
-      }}
-      onMouseLeave={() => {
-        setShowServices(false)
-      }}
-    >
-      <span>Servicios</span>
-      <ul
-        className={`absolute bg-white/50 rounded-2xl shadow-[0_4px_30px_rgba(0,_0,_0,_0.1)] backdrop-blur-[10px] border-[1px] border-solid border-white/30 py-3 text-white flex flex-col gap-2 left-[50%] text-center transition-transform duration-[250ms] ${
-          showServices
-            ? 'scale-100 translate-x-[-50%]'
-            : 'scale-0 translate-x-[-50%]'
-        } origin-top shadow-lg`}
+    <Menu as="div" className="relative inline-block text-left">
+      <div>
+        <Menu.Button className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-transparent px-3 py-2 text-white hover:text-blue-700">
+        Servicios
+        </Menu.Button>
+      </div>
+
+      <Transition
+        as={Fragment}
+        enter="transition ease-out duration-100"
+        enterFrom="transform opacity-0 scale-95"
+        enterTo="transform opacity-100 scale-100"
+        leave="transition ease-in duration-75"
+        leaveFrom="transform opacity-100 scale-100"
+        leaveTo="transform opacity-0 scale-95"
       >
-        {links.map((link) => (
-          <li
-            key={link.title}
-            className="hover:text-blue-700 px-2 font-normal text-black"
-          >
-            <Link href={link.href}>{link.title}</Link>
-          </li>
-        ))}
-      </ul>
-    </div>
+        <Menu.Items className="absolute left-1 right-0 z-10 mt-2 w-32 origin-top-right divide-y divide-gray-700 rounded-md bg-slate-900 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+          <div className="py-1">
+            <Menu.Item>
+              {({ active }) => (
+                <Link
+                  href="#"
+                  className={classNames(
+                    active ? 'bg-sky-900/50 text-white' : 'text-white',
+                    'block px-4 py-2 text-sm'
+                  )}
+                >
+                  Restaurante
+                </Link>
+              )}
+            </Menu.Item>
+            <Menu.Item>
+              {({ active }) => (
+                <Link
+                  href="/tenis/horario"
+                  className={classNames(
+                    active ? 'bg-sky-900/50 text-white' : 'text-white',
+                    'block px-4 py-2 text-sm'
+                  )}
+                >
+                  Tenis
+                </Link>
+              )}
+            </Menu.Item>
+            <Menu.Item>
+              {({ active }) => (
+                <Link
+                  href="#"
+                  className={classNames(
+                    active ? 'bg-sky-900/50 text-white' : 'text-white',
+                    'block px-4 py-2 text-sm'
+                  )}
+                >
+                  Informática
+                </Link>
+              )}
+            </Menu.Item>
+            <Menu.Item>
+              {({ active }) => (
+                <Link
+                  href="#"
+                  className={classNames(
+                    active ? 'bg-sky-900/50 text-white' : 'text-white',
+                    'block px-4 py-2 text-sm'
+                  )}
+                >
+                  Pádel
+                </Link>
+              )}
+            </Menu.Item>
+          </div>
+        </Menu.Items>
+      </Transition>
+    </Menu>
   )
 }
 
-export default Services
+export default Profile
