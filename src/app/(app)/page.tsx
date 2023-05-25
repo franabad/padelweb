70d30a2be95866c1fc311acc80a9a47348bcef69
Fran Abad <fam1096@hotmail.com>
2023-05-25 02:18:54 +0200

    Add hero main page

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -1,13 +1,12 @@
 // import { getServerSession } from 'next-auth'
 // import { authOptions } from '../../pages/api/auth/[...nextauth]'
 
+import Hero from '@/components/server/hero'
 const Page = () => {
   // const session = await getServerSession(authOptions)
 
   return (
-    <main className="justify-center items-center flex pt-[70px]">
-      <h1 className="text-white text-6xl animate-fade-up animate-once animate-duration-1000 animate-delay-300 animate-ease-in-out">Esports Center</h1>
-    </main>
+    <Hero />
   )
 }
 
