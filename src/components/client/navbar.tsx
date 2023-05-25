674c821fca250bdb45a613b6732894ffb12b432a
Fran Abad <fam1096@hotmail.com>
2023-05-25 21:40:00 +0200

    Add logo and title in navbar

--- src/components/client/navbar.tsx
+++ src/components/client/navbar.tsx
@@ -2,11 +2,18 @@
 import Link from 'next/link'
 import Services from './services'
 import { type ReactNode } from 'react'
+import Image from 'next/image'
 
 const NavBar = ({ children }: { children: ReactNode }) => {
   return (
     <nav className="bg-slate-900 w-full flex h-[70px] justify-between items-center navbar fixed z-50">
       <div className="list-none flex justify-end items-center flex-1">
+        <div className='ml-5 flex items-center'>
+          <Link href="/">
+            <Image alt="logo" src='/assets/logo.webp' width={70} height={70} className="image-link"></Image>
+          </Link>
+          <p className='text-white font-semibold text-3xl ml-2'>e-Sports Center</p>
+        </div>
         <ul className="list-none flex justify-end items-center flex-1">
           <li>
             <Link
