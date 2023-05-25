70e3602fd3a3276cfa0277c876e84765130e061c
Fran Abad <fam1096@hotmail.com>
2023-05-26 00:44:34 +0200

    Update hero buttons

--- src/components/server/hero.tsx
+++ src/components/server/hero.tsx
@@ -7,8 +7,8 @@ const Hero = () => {
       <header className='flex flex-col items-center fixed top-0 w-full z-40 pt-[70px]'>
         <div className='mt-40 flex flex-col justify-evenly'>
           <h1 className="text-white text-9xl animate-fade-up animate-once animate-duration-1000 animate-delay-100 animate-ease-in-out delay-200">Esports <span className='text-yellow-700'>Center</span></h1>
-          <div className="flex justify-evenly mt-20 animate-fade-up animate-once animate-duration-1000 animate-delay-700 animate-ease-in-out delay-[2000ms] ">
-            <Link href="/register" className="bg-sky-500 tex-white py-2 px-5 text-xl rounded-md hover:bg-sky-900">Empieza ya</Link>
+          <div className="flex justify-center mt-20 gap-24 animate-fade-up animate-once animate-duration-1000 animate-delay-700 animate-ease-in-out delay-[2000ms] ">
+            <Link href="/register" className="bg-sky-500 tex-white py-2 px-5 text-xl rounded-md hover:bg-sky-900 ">Empieza ya</Link>
             <Link href="/precios" className="bg-white/80 text-black py-2 px-5 text-xl rounded-md hover:bg-black hover:text-gray-100">Más información</Link>
           </div>
         </div>
