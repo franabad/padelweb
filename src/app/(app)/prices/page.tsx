501d4e0b27923e4ad64327254fde8ad637eb26a7
Fran Abad <fam1096@hotmail.com>
2023-05-26 16:20:56 +0200

    Change dirs names

---
+++ src/app/(app)/prices/page.tsx
@@ -0,0 +1,12 @@
+// import { getServerSession } from 'next-auth'
+// import { authOptions } from '../../../pages/api/auth/[...nextauth]'
+
+const Page = async () => {
+  return (
+    <div className="flex justify-center items-center w-screen h-screen">
+      <p className="text-red-800 pt-[70px] text-4xl">Work in progress</p>
+    </div>
+  )
+}
+
+export default Page
