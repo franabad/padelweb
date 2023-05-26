820a99cbd6047fb3f2ada7cfd2f223d55849099f
Fran Abad <fam1096@hotmail.com>
2023-05-26 18:02:32 +0200

    Change prices hero button

--- src/components/server/hero.tsx
+++ src/components/server/hero.tsx
@@ -9,7 +9,7 @@ const Hero = () => {
           <h1 className="text-white text-9xl animate-fade-up animate-once animate-duration-1000 animate-delay-100 animate-ease-in-out delay-200">Esports <span className='text-yellow-700'>Center</span></h1>
           <div className="flex justify-center mt-20 gap-24 animate-fade-up animate-once animate-duration-1000 animate-delay-700 animate-ease-in-out delay-[2000ms] ">
             <Link href="/register" className="bg-sky-500 tex-white py-2 px-5 text-xl rounded-md hover:bg-sky-900 ">Empieza ya</Link>
-            <Link href="/precios" className="bg-white/80 text-black py-2 px-5 text-xl rounded-md hover:bg-black hover:text-gray-100">Más información</Link>
+            <Link href="/prices" className="bg-white/80 text-black py-2 px-5 text-xl rounded-md hover:bg-black hover:text-gray-100">Más información</Link>
           </div>
         </div>
       </header>
