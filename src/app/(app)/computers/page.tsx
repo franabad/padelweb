d9f0b274cd5e55b2c0a7088e700a1096cd776ac2
Fran Abad <fam1096@hotmail.com>
2023-05-26 16:34:39 +0200

    Add mini-text to computers page

--- src/app/(app)/computers/page.tsx
+++ src/app/(app)/computers/page.tsx
@@ -6,7 +6,9 @@ const Page = async () => {
 
   // console.log('Session en about page', session)
   return (
-    <div className="flex justify-center items-center w-screen h-screen">
+    <div className="flex flex-col justify-center items-center w-screen h-screen">
+      <p className="text-white mb-5">Regístrate en la web y con esas mismas credenciales, podrás acercarte al centro y disfrutar del área de ordenadores, con las últimas actualizaciones del mercado y los últimos videojuegos instalados.</p>
+      <p className="text-white">Gratis por tiempo limitado.</p>
       <p className="text-red-800 pt-[70px] text-4xl">Work in progress</p>
     </div>
   )
